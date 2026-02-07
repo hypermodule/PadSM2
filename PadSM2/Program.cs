842eea1b2e4e7b39d6558e3a647918c41c3f4b8b
hyper <hypermodule@users.noreply.github.com>
2026-02-07 12:56:13 +0100

    Clean up a bit

--- PadSM2/Program.cs
+++ PadSM2/Program.cs
@@ -1,9 +1,3 @@
-using CUE4Parse.FileProvider;
-using CUE4Parse.MappingsProvider;
-using CUE4Parse.UE4.Versions;
-using Newtonsoft.Json;
-using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
 using UAssetAPI;
 using UAssetAPI.UnrealTypes;
 
@@ -12,12 +6,6 @@ namespace PadSM2;
 public class Program
 {
     public static void Main(string[] args)
-    {
-        Debug();
-        //ManualTest(args);
-    }
-
-    public static void ManualTest(string[] args)
     {
         Console.WriteLine("PadSM2 v0.0.2");
 
@@ -37,48 +25,44 @@ public class Program
         var convertedAsset = MeshConverter.ConvertToAugusta(paddedUasset, paddedUexp);
 
         var tmpDir = Directory.CreateTempSubdirectory();
+        
         var tmpUassetPath = Path.Join(tmpDir.FullName, "converted.uasset");
         var tmpUexpPath = Path.ChangeExtension(tmpUassetPath, ".uexp");
         convertedAsset.Write(tmpUassetPath);
         
-        var tmpUasset = ByteAsset.Read(tmpUassetPath);
-        var tmpUexp = ByteAsset.Read(tmpUexpPath);
+        var unversionedUasset = ByteAsset.Read(tmpUassetPath);
+        var unversionedUexp = ByteAsset.Read(tmpUexpPath);
         
         var augustaUsmap = MeshConverter.GetUsmap("PadSM2.Augusta.usmap");
-        var x = MeshConverter.ParseAsset(tmpUasset, tmpUexp, augustaUsmap, CustomSerializationFlags.NoDummies);
-        x.PackageFlags &= ~EPackageFlags.PKG_UnversionedProperties;
-        PropertyTypeNameFixer.Populate(x);
-        x.AddNameReference(new FString("None"));
-        var outDir = Path.Join(tmpDir.FullName, "out");
-        Directory.CreateDirectory(outDir);
-        var outPath = Path.Join(outDir, Path.GetFileName(uassetPath));
-        x.Write(outPath);
-        Console.WriteLine("wrote " + outPath);
+        var versionedAsset = MeshConverter.ParseAsset(unversionedUasset, unversionedUexp, augustaUsmap, CustomSerializationFlags.NoDummies);
+        versionedAsset.PackageFlags &= ~EPackageFlags.PKG_UnversionedProperties;
+        PropertyTypeNameFixer.Populate(versionedAsset);
+        versionedAsset.AddNameReference(new FString("None"));
 
+        var outputDir = Path.GetDirectoryName(uassetPath);
+        if (string.IsNullOrEmpty(outputDir)) outputDir = ".";
+        var tmpOutBase = Path.GetFileNameWithoutExtension(uassetPath) + ".padsm2.tmp." + Random.Shared.Next();
+        var tmpOutPath = Path.Join(outputDir, tmpOutBase + ".uasset");
+        var tmpOutUexpPath = Path.ChangeExtension(tmpOutPath, ".uexp");
 
-        /*
-        File.Move(uassetPath, Path.ChangeExtension(uassetPath, ".uasset.bak"));
-        File.Move(uexpPath, Path.ChangeExtension(uexpPath, ".uexp.bak"));
-        */
-    }
+        versionedAsset.Write(tmpOutPath);
 
-    public static void Debug()
-    {
-        Log.Logger = new LoggerConfiguration().WriteTo.Console(theme: AnsiConsoleTheme.Literate).CreateLogger();
-        
-        const string _archiveDirectory = "/home/hyper/.local/share/Steam/steamapps/common/Grounded2/Augusta/Content/Paks/";
-        const string _mapping = "/home/hyper/.local/share/Steam/steamapps/common/Grounded2/Augusta.usmap";
-        const string assetPath = "Game/Content/Art/World/ZN00_Global/Coins/Coin_Quarter/SM_Coin_Quarter_A.SM_Coin_Quarter_A";
-        
-        var version = new VersionContainer(EGame.GAME_Grounded2);
-        var provider = new DefaultFileProvider(_archiveDirectory, SearchOption.AllDirectories, version)
+        var uassetBakPath = Path.ChangeExtension(uassetPath, ".uasset.bak");
+        var uexpBakPath = Path.ChangeExtension(uexpPath, ".uexp.bak");
+
+        if (File.Exists(uassetPath))
         {
-            //MappingsContainer = new FileUsmapTypeMappingsProvider(_mapping)
-        };
-        provider.Initialize();
-        provider.Mount();
-        
-        var obj = provider.LoadPackageObject(assetPath);
-        Console.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
+            File.Move(uassetPath, uassetBakPath, true);
+        }
+        if (File.Exists(uexpPath))
+        {
+            File.Move(uexpPath, uexpBakPath, true);
+        }
+
+        File.Move(tmpOutPath, uassetPath, true);
+        if (File.Exists(tmpOutUexpPath))
+        {
+            File.Move(tmpOutUexpPath, uexpPath, true);
+        }
     }
 }
